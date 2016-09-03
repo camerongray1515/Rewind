5c30d5046649787f30e4b7fa944d7f5d489efc27
Cameron Gray <cameron@camerongray.me>
2016-09-04 00:36:50 +0100

    Added manpage, removed some unused includes

--- src/rewind.cpp
+++ src/rewind.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <stdio.h>
-#include <stdlib.h>
 #include <boost/program_options.hpp>
-#include <ctime>
 #include <iomanip>
 
 std::string filename = "/tmp/pending-rollback.rewind";
